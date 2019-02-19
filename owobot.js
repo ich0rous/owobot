7ddbc956423c439160568282050643fbe5467423
octazis <merceeclient@gmail.com>
2019-02-19 23:21:53 +0000

    Update owobot.js

--- owobot.js
+++ owobot.js
@@ -14,15 +14,15 @@ bot.on('message', msg => {
 
   if(msg.content.startsWith("owo!")){ // Check for prefix.
     let cmd = msg.content.split("owo!") // Split at the prefix.
+    console.log(cmd[0]+cmd[1])
     if(cmd[1] == "?"){ // cmd[1] returns with the actual command.
       msg.channel.sendMessage("OwO, what's this?"); // Respond accordingly.
-    } else if(cmd[1] == "" && cmd[0].toUpperCase() == "OWO!") { // Bodgey.
+    } else if(cmd[1] == "img") { // Suck my cock.
       msg.channel.send({
-        files: [{
-          attachment: './triggerowo_by_alziratt-dcclp1e.gif',
-          name: 'owo.gif'
-        }]
+        files: ['https://puu.sh/COkZQ/6dd8c62320.gif'] // Send the remote image.
       })
+        .then(console.log) // Stop complaining node.
+        .catch(console.error);
     }
   }
 
